21f45838a6a04d49c769f64e32b20caad512eac6
Jeffrey N. Johnson <jeff@cohere-llc.com>
2024-09-12 10:44:12 -0700

    Preventing PartMC from searching for libraries outside our build environment.

--- partmc/netcdf.cmake
+++ partmc/netcdf.cmake
@@ -1,27 +1,6 @@
-find_path(NETCDF_INCLUDE_DIR netcdf.mod NETCDF.mod
-  DOC "NetCDF include directory (must contain netcdf.mod)"
-  PATHS
-  $ENV{NETCDF_HOME}/include
-  /usr/lib/gfortran/modules
-  /usr/lib64/gfortran/modules
-  /opt/local/include)
-find_library(Z_LIB z
-  DOC "libz"
-  PATHS $ENV{NETCDF_HOME}/lib /opt/local/lib)
-find_library(HDF5_LIB hdf5
-  DOC "HDF5 library"
-  PATHS $ENV{NETCDF_HOME}/lib /opt/local/lib)
-find_library(HDF5_HL_LIB hdf5_hl
-  DOC "HDF5 high-level library"
-  PATHS $ENV{NETCDF_HOME}/lib /opt/local/lib)
-find_library(NETCDF_C_LIB netcdf
-  DOC "NetCDF C library"
-  PATHS $ENV{NETCDF_HOME}/lib /opt/local/lib)
-find_library(NETCDF_FORTRAN_LIB netcdff
-  DOC "NetCDF Fortran library"
-  PATHS $ENV{NETCDF_HOME}/lib /opt/local/lib)
-set(NETCDF_LIBS ${NETCDF_C_LIB})
+# Instead of using CMake's find_library, which introduces ridiculous externalities,
+# we simply pass in the relevant paths as regular CMake variables. -JNJ
 if(NETCDF_FORTRAN_LIB)
-  set(NETCDF_LIBS ${NETCDF_FORTRAN_LIB} ${NETCDF_LIBS} ${HDF5_HL_LIB} ${HDF5_LIB} ${Z_LIB})
+  set(NETCDF_LIBS ${NETCDF_FORTRAN_LIB} ${NETCDF_FORTRAN_LIB} ${NETCDF_C_LIB} ${HDF5_HL_LIB} ${HDF5_LIB} ${Z_LIB})
 endif()
 include_directories(${NETCDF_INCLUDE_DIR})
